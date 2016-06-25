62a6bc2f4dd4e9e1297b7943fc4604a8734f6383
Mark Ide <mark@cranstonide.com>
2016-06-25 10:49:12 -0400

    Move download of minecraft client to entrypoint

---
+++ entrypoint.sh
@@ -0,0 +1,3 @@
+wget https://s3.amazonaws.com/Minecraft.Download/versions/${MINECRAFT_VERSION}/${MINECRAFT_VERSION}.jar -P /home/minecraft/.minecraft/versions/${MINECRAFT_VERSION}/
+
+overviewer.py --config /home/minecraft/config.py
