7ccfb88e093242d6f5199e0f0be3bbcfcbb09531
Mark Ide <mide@imprivata.com>
2016-06-25 11:02:19 -0400

    Add POI render

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,4 @@
 wget https://s3.amazonaws.com/Minecraft.Download/versions/${MINECRAFT_VERSION}/${MINECRAFT_VERSION}.jar -P /home/minecraft/.minecraft/versions/${MINECRAFT_VERSION}/
 
 overviewer.py --config /home/minecraft/config.py
+overviewer.py --config /home/minecraft/config.py --genpoi
