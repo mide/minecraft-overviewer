2d7974ac4895af5e7d2f5a627656bb3edbfa65a9
Mark Ide <mark@cranstonide.com>
2016-07-10 08:57:51 -0400

    Add filter text to signs

--- config/config.py
+++ config/config.py
@@ -3,9 +3,12 @@ def playerIcons(poi):
         poi['icon'] = "http://overviewer.org/avatar/%s" % poi['EntityId']
         return "Last known location for %s" % poi['EntityId']
 
+# Only signs with "-- RENDER --" on the last line will be shown
+# Otherwise, people can't have secret bases and the render is too busy anyways.
 def signFilter(poi):
     if poi['id'] == 'Sign':
-        return "\n".join([poi['Text1'], poi['Text2'], poi['Text3'], poi['Text4']])
+        if poi['Text4'] == '-- RENDER --':
+            return "\n".join([poi['Text1'], poi['Text2'], poi['Text3'], poi['Text4']])
 
 worlds['minecraft'] = "/home/minecraft/server/world"
 outputdir = "/home/minecraft/render/"
