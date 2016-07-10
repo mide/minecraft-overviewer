17bdcbc6a813d0b1d6e078bd9f854d47bb61a539
Mark Ide <mark@cranstonide.com>
2016-07-10 08:28:59 -0400

    Render signs

--- config/config.py
+++ config/config.py
@@ -3,19 +3,28 @@ def playerIcons(poi):
         poi['icon'] = "http://overviewer.org/avatar/%s" % poi['EntityId']
         return "Last known location for %s" % poi['EntityId']
 
+def signFilter(poi):
+    if poi['id'] == 'Sign':
+        return "\n".join([poi['Text1'], poi['Text2'], poi['Text3'], poi['Text4']])
+
 worlds['minecraft'] = "/home/minecraft/server/world"
 outputdir = "/home/minecraft/render/"
+
+markers = [
+    dict(name="Players", filterFunction=playerIcons),
+    dict(name="Signs", filterFunction=signFilter)
+]
 
 renders["day"] = {
     'world': 'minecraft',
     'title': 'Day',
     'rendermode': 'smooth_lighting',
-    'markers': [dict(name="Players", filterFunction=playerIcons)]
+    'markers': markers
 }
 
 renders["night"] = {
     'world': 'minecraft',
     'title': 'Night',
     'rendermode': 'smooth_night',
-    'markers': [dict(name="Players", filterFunction=playerIcons)]
+    'markers': markers
 }
