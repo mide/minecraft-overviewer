39c8af984c69ddafd2b40aa562e6ee184269e190
Mark Ide <mide@imprivata.com>
2017-01-19 20:40:16 -0500

    Comments

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 set -o errexit
 
+# Require MINECRAFT_VERSION environment variable to be set (no default assumed)
 if [ -z "$MINECRAFT_VERSION" ]; then
   echo "Expecting environment variable MINECRAFT_VERSION to be set to non-empty string. Exiting."
   exit 1
 fi
 
+# Download Minecraft client .jar (Contains textures used by Minecraft Overviewer)
 wget https://s3.amazonaws.com/Minecraft.Download/versions/${MINECRAFT_VERSION}/${MINECRAFT_VERSION}.jar -P /home/minecraft/.minecraft/versions/${MINECRAFT_VERSION}/
 
+# Run the world renders (One pass to make map, one to generate points of interests)
 overviewer.py --config /home/minecraft/config.py
 overviewer.py --config /home/minecraft/config.py --genpoi
