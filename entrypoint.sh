8f65270ab271b80da1bea0806c11b0fbd484dc91
Spencer Gardner <snppla@gmail.com>
2017-09-07 13:27:22 -0600

    Added ability to run poi generation only
    Added ability to inject google map api key

--- entrypoint.sh
+++ entrypoint.sh
@@ -11,10 +11,13 @@ fi
 wget -N https://s3.amazonaws.com/Minecraft.Download/versions/${MINECRAFT_VERSION}/${MINECRAFT_VERSION}.jar -P /home/minecraft/.minecraft/versions/${MINECRAFT_VERSION}/
 
 # Run the world renders (One pass to make map, one to generate points of interests)
-overviewer.py --config /home/minecraft/config.py
-overviewer.py --config /home/minecraft/config.py --genpoi
 
-# Add Google Maps API key if the GOOGLE_MAPS_API_KEY environment variable is set
-if [ "$GOOGLE_MAPS_API_KEY" ]; then
-  sed -i "s|https://maps.google.com/maps/api/js|&?key=${GOOGLE_MAPS_API_KEY}|g" /home/minecraft/render/index.html
+if [ "$POI_ONLY" != "y" ]; then
+  overviewer.py --config /home/minecraft/config.py
+  # Add Google Maps API key if the GOOGLE_MAPS_API_KEY environment variable is set
+  if [ "$GOOGLE_MAPS_API_KEY" ]; then
+    sed -i "s|https://maps.google.com/maps/api/js|&?key=${GOOGLE_MAPS_API_KEY}|g" /home/minecraft/render/index.html
+  fi
 fi
+
+overviewer.py --config /home/minecraft/config.py --genpoi
