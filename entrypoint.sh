650e0b9cc5e03d9547268d103251557a9cc981ff
Patrick Lucas <me@patricklucas.com>
2019-03-03 18:00:31 +0100

    Explicitly set texturepath in config file
    Closes #29

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 set -o errexit
 
+CONFIG=/home/minecraft/config.py
+
 # Require MINECRAFT_VERSION environment variable to be set (no default assumed)
 if [ -z "$MINECRAFT_VERSION" ]; then
   echo "Expecting environment variable MINECRAFT_VERSION to be set to non-empty string. Exiting."
@@ -11,13 +13,14 @@ fi
 CLIENT_URL=$(python /home/minecraft/download_url.py "$MINECRAFT_VERSION")
 echo "Using Client URL $CLIENT_URL."
 wget -N "${CLIENT_URL}" -O "${MINECRAFT_VERSION}.jar" -P /home/minecraft/.minecraft/versions/${MINECRAFT_VERSION}/
+echo "texturepath = '/home/minecraft/${MINECRAFT_VERSION}.jar'" >> "$CONFIG"
 
 # Render the Map
 if [ "$RENDER_MAP" == "true" ]; then
-  overviewer.py --config /home/minecraft/config.py $ADDITIONAL_ARGS
+  overviewer.py --config "$CONFIG" $ADDITIONAL_ARGS
 fi
 
 # Render the POI
 if [ "$RENDER_POI" == "true" ]; then
-  overviewer.py --config /home/minecraft/config.py --genpoi $ADDITIONAL_ARGS
+  overviewer.py --config "$CONFIG" --genpoi $ADDITIONAL_ARGS
 fi
