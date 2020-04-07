70c72b1dd9ec457f7fe61c3f6c2a6f52d42f39a6
Mark Ide <546518+mide@users.noreply.github.com>
2020-04-07 00:53:13 -0400

    After testing

--- config/config.py
+++ config/config.py
@@ -13,11 +13,13 @@ def playerIcons(poi):
 # reasons, but also so that people can have secret bases and to keep the render
 # fairly clean.
 def signFilter(poi):
+    # Because of how Overviewer reads this file, we must "import os" again here.
+    import os
     if poi['id'] in ['Sign', 'minecraft:sign']:
         sign_filter_string = os.environ.get('RENDER_SIGNS_INCLUDE', "")
         render_all_signs = len(sign_filter_string) == 0
         if render_all_signs or sign_filter_string in poi.values():
-            return "\n".join([poi['Text1'], poi['Text2'], poi['Text3'], poi['Text4']])
+            return "<br>".join([poi['Text1'], poi['Text2'], poi['Text3'], poi['Text4']])
 
 
 worlds['minecraft'] = "/home/minecraft/server/world"
