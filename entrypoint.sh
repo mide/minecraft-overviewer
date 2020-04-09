12982682e64308a440eae3e7eb51a28df9e45290
Mark Ide <546518+mide@users.noreply.github.com>
2020-04-08 21:53:35 -0400

    Allow for Client Jar Caching (#66)
    This will allow for the client .jar file to be cached by power uses (normal users aren't impacted). 
    
    This will impact anyone who uses a custom configuration which has the "texturepath" line in it. It's likely that if a user has a custom config, they have that option set since we used to have the .jar in a non-default location. For those users, we display a warning and copy the .jar into the old locations.
    
    In the future (July) we will remove the workaround and force users to update their configurations.

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,9 +8,36 @@ if [ -z "$MINECRAFT_VERSION" ]; then
 fi
 
 # Download Minecraft client .jar (Contains textures used by Minecraft Overviewer)
-CLIENT_URL=$(python3 /home/minecraft/download_url.py "$MINECRAFT_VERSION")
-echo "Using Client URL $CLIENT_URL."
-wget -N "${CLIENT_URL}" -O "${MINECRAFT_VERSION}.jar" -P /home/minecraft/.minecraft/versions/${MINECRAFT_VERSION}/
+# We only download if the client doesn't exist. In most cases, it won't exist
+# because the directory isn't a volume and the Docker container will be a fresh
+# slate. This check enables power-users to mount the /home/minecraft/.minecraft/
+# directory and prevent downloading if the file exists.
+if [ -f "/home/minecraft/.minecraft/versions/${MINECRAFT_VERSION}/${MINECRAFT_VERSION}.jar" ]; then
+    echo "Minecraft client ${MINECRAFT_VERSION}.jar exists! Skipping download."
+else
+    CLIENT_URL=$(python3 /home/minecraft/download_url.py "${MINECRAFT_VERSION}")
+    echo "Using Client URL ${CLIENT_URL} to download ${MINECRAFT_VERSION}.jar."
+    mkdir -p "/home/minecraft/.minecraft/versions/${MINECRAFT_VERSION}/"
+    wget "${CLIENT_URL}" -O "/home/minecraft/.minecraft/versions/${MINECRAFT_VERSION}/${MINECRAFT_VERSION}.jar"
+fi
+
+# DEPRECATION WARNING - TODO: Remove this block on or around July 31, 2020.
+if grep -q "texturepath" "${CONFIG_LOCATION}"; then
+  NEW_LOCATION="/home/minecraft/.minecraft/versions/${MINECRAFT_VERSION}/${MINECRAFT_VERSION}.jar"
+  OLD_LOCATION="/home/minecraft/${MINECRAFT_VERSION}.jar"
+  echo "----------------------------------------------------------------------"
+  echo "                         DEPRECATION WARNING!                         "
+  echo "----------------------------------------------------------------------"
+  echo "The texture file location has changed and 'texturepath' set in your   "
+  echo "config.py! Either update your configuration to point to the new       "
+  echo "location, or remove that line. We will copy the texturepath into the  "
+  echo "old location for now. On or around July 31, 2020, we will drop support"
+  echo "for the old location.                                                 "
+  echo "Old Location: ${OLD_LOCATION}                                         "
+  echo "New Location: ${NEW_LOCATION}                                         "
+  echo "----------------------------------------------------------------------"
+  cp "${NEW_LOCATION}" "${OLD_LOCATION}"
+fi
 
 # Render the Map
 if [ "$RENDER_MAP" == "true" ]; then
