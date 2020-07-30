0329fcd3359f24e0546c054b3d0ac04b619a21b2
Mark Ide <546518+mide@users.noreply.github.com>
2020-07-29 20:57:52 -0400

    shellcheck

--- entrypoint.sh
+++ entrypoint.sh
@@ -41,12 +41,14 @@ fi
 
 # Render the Map
 if [ "$RENDER_MAP" == "true" ]; then
-  # shellcheck disable=SC2086 (We disable to support passing multiple arguments)
+  # (We disable to support passing multiple arguments)
+  # shellcheck disable=SC2086
   overviewer.py --config "$CONFIG_LOCATION" $ADDITIONAL_ARGS
 fi
 
 # Render the POI
 if [ "$RENDER_POI" == "true" ]; then
-  # shellcheck disable=SC2086 (We disable to support passing multiple arguments)
+# (We disable to support passing multiple arguments)
+  # shellcheck disable=SC2086
   overviewer.py --config "$CONFIG_LOCATION" --genpoi $ADDITIONAL_ARGS_POI
 fi
