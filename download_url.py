52ad751c6804dc57a8f3136ad022350ea2a78a7c
Mark Ide <546518+mide@users.noreply.github.com>
2021-03-21 19:30:03 -0400

    linting

--- download_url.py
+++ download_url.py
@@ -11,7 +11,9 @@ MANIFEST_URL = "https://launchermeta.mojang.com/mc/game/version_manifest.json"
 
 def get_json_from_url(url):
     if not url.startswith("https://"):
-        raise RuntimeError("Expected URL to start with https://. It was '{}'.".format(url))
+        raise RuntimeError(
+            "Expected URL to start with https://. It was '{}'.".format(url)
+        )
     request = urllib.request.Request(url)
     response = urllib.request.urlopen(request)
     return json.loads(response.read().decode())
@@ -23,11 +25,15 @@ def get_minecraft_download_url(version):
     desired_versions = list(filter(lambda v: v["id"] == version, data["versions"]))
     if len(desired_versions) == 0:
         raise RuntimeError(
-            "Couldn't find Minecraft Version {version} in manifest file {MANIFEST_URL}.".format(version, MANIFEST_URL)
+            "Couldn't find Minecraft Version {version} in manifest file {MANIFEST_URL}.".format(
+                version, MANIFEST_URL
+            )
         )
     elif len(desired_versions) > 1:
         raise RuntimeError(
-            "Found more than one record published for version {version} in manifest file {MANIFEST_URL}.".format(version, MANIFEST_URL)
+            "Found more than one record published for version {version} in manifest file {MANIFEST_URL}.".format(
+                version, MANIFEST_URL
+            )
         )
 
     version_manifest_url = desired_versions[0]["url"]
