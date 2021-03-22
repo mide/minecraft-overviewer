bf942d776e14ee4398f38392e0c33b1a912ca025
Mark Ide <546518+mide@users.noreply.github.com>
2021-03-21 21:41:14 -0400

    Cleaning

--- download_url.py
+++ download_url.py
@@ -25,11 +25,15 @@ def get_minecraft_download_url(version):
     desired_versions = list(filter(lambda v: v["id"] == version, data["versions"]))
     if len(desired_versions) == 0:
         raise RuntimeError(
-            "Couldn't find Minecraft Version {} in manifest file {}.".format(version, MANIFEST_URL)
+            "Couldn't find Minecraft Version {} in manifest file {}.".format(
+                version, MANIFEST_URL
+            )
         )
     elif len(desired_versions) > 1:
         raise RuntimeError(
-            "Found more than one record published for version {} in manifest file {}.".format(version, MANIFEST_URL)
+            "Found more than one record published for version {} in manifest file {}.".format(
+                version, MANIFEST_URL
+            )
         )
 
     version_manifest_url = desired_versions[0]["url"]
